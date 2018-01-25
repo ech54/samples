c15e5fc1fcfdadada74164431e6157bffc6775f6
Emilien Charton <emilien.charton@neoxam.com>
2018-01-25 12:44:17 +0100

    setup spring-boot and eureka"

--- java/samples-netflix/netflix-userdetails/src/main/java/org/ech/samples/netflix/UserDetailsApplication.java
+++ java/samples-netflix/netflix-userdetails/src/main/java/org/ech/samples/netflix/UserDetailsApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@EnableAutoConfiguration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserDetailsApplication {
 	
