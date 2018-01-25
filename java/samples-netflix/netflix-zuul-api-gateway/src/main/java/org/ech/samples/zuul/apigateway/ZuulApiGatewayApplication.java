241da33e561be968d40c95753ed931b4a4d4710a
Emilien Charton <emilien.charton@neoxam.com>
2018-01-25 17:24:54 +0100

    add zuul configuration

---
+++ java/samples-netflix/netflix-zuul-api-gateway/src/main/java/org/ech/samples/zuul/apigateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package org.ech.samples.zuul.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+}
